2bb1b42422138a9042fed87632a21ece579d1389
Cristian Iosif <cristianiosif@me.com>
2020-05-03 13:54:32 +0300

    Initial commit

--- webpack.mix.js
+++ webpack.mix.js
@@ -4,3 +4,6 @@ require("laravel-mix-tailwind");
 mix.react("resources/js/app.js", "public/js")
     .sass("resources/sass/app.scss", "public/css")
     .tailwind();
+// .browserSync({
+//     proxy: "https://dfl-clone.test",
+// });
