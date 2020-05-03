091173a4b3dbf0c4e28469f987ba34f688cd4fad
Cristian Iosif <cristianiosif@me.com>
2020-05-03 19:00:54 +0300

    Homepage for mobile

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,6 @@
 const mix = require("laravel-mix");
 
-mix.react("resources/js/app.js", "public/js")
+mix.js("resources/js/app.js", "public/js")
     .postCss("resources/css/main.css", "public/css", [require("tailwindcss")])
     .browserSync({
         proxy: "http://dfl-clone.test",
